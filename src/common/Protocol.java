94c26d04ad997c01d07a8fd73c99b4f40b771092
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2018-10-24 13:03:14 +0200

    Dummy implementations of Protocol and PersistenceService for testing the server

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -1,7 +1,11 @@
 package common;
 
 import common.exceptions.ProtocolException;
+import common.messages.DefaultKVMessage;
 import common.messages.KVMessage;
+import common.utils.RecordReader;
+
+import java.io.IOException;
 
 /**
  * Implementation of the wire protocol.
@@ -16,8 +20,14 @@ public final class Protocol {
      * @return encoded data as per protocol
      */
     public static byte[] encode(KVMessage message) {
-        // TODO
-        return null;
+        // TODO replace with real implementation
+        String combined = "UNSUPPORTED OPERATION\n";
+        if (message.getStatus() == KVMessage.StatusType.GET_SUCCESS) {
+            combined = String.format("GET_SUCCESS: %s -> %s\n", message.getKey(), message.getValue());
+        } else if (message.getStatus() == KVMessage.StatusType.PUT_SUCCESS) {
+            combined = String.format("PUT_SUCCESS: %s -> %s\n", message.getKey(), message.getValue());
+        }
+        return combined.getBytes();
     }
 
     // TODO: decide if we want to do this
@@ -38,8 +48,25 @@ public final class Protocol {
      * @throws ProtocolException if the data actually encoded an exception
      */
     public static KVMessage decode(byte[] payload) throws ProtocolException {
-        // TODO
-        return null;
+        // TODO replace with real implementation
+        try {
+            RecordReader fieldReader = new RecordReader(payload, (byte) ' ');
+
+            String operationCode = new String(fieldReader.read());
+
+            if ("PUT".equals(operationCode)) {
+                String key = new String(fieldReader.read());
+                String value = new String(fieldReader.read());
+                return new DefaultKVMessage(key, value, KVMessage.StatusType.PUT);
+            } else if ("GET".equals(operationCode)) {
+                String key = new String(fieldReader.read());
+                return new DefaultKVMessage(key, null, KVMessage.StatusType.GET);
+            } else {
+                throw new ProtocolException("Unknown op code: " + operationCode);
+            }
+        } catch (NullPointerException | IOException e) {
+            throw new ProtocolException("Could not decode message");
+        }
     }
 
 }
