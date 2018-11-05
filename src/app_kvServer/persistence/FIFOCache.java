c49a2e3b0dbb8d55c53c64bb8621e67b60ed7bbf
Xhens Basha <basha@agrando.de>
2018-11-05 18:46:34 +0100

    add javadocs

--- src/app_kvServer/persistence/FIFOCache.java
+++ src/app_kvServer/persistence/FIFOCache.java
@@ -7,10 +7,21 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+/**
+ * https://effective-java.com/2011/05/simple-lru-or-fifo-cache/
+ *
+ * @param <K> Type of the keys
+ * @param <V> Type of the values
+ */
 public class FIFOCache<K, V> implements Cache<K, V> {
     private static final Logger LOG = LogManager.getLogger(FIFOCache.class);
     private final Map<K, V> cacheMap;
 
+    /**
+     * Default constructor
+     *
+     * @param cacheSize Number of elements to hold
+     */
     public FIFOCache(int cacheSize) {
         this.cacheMap = new LinkedHashMap<>(cacheSize, 0.75f, false) {
             @Override
@@ -21,6 +32,12 @@ public class FIFOCache<K, V> implements Cache<K, V> {
         };
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param key
+     * @return value of the key
+     */
     @Override
     public V get(K key) {
         if (cacheMap.get(key) == null) {
@@ -29,16 +46,33 @@ public class FIFOCache<K, V> implements Cache<K, V> {
         return cacheMap.get(key);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param key
+     * @param value
+     */
     @Override
     public void put(K key, V value) {
         cacheMap.put(key, value);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param key
+     * @return boolean if the key is contained
+     */
     @Override
     public boolean contains(K key) {
         return cacheMap.containsKey(key);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @param key
+     */
     @Override
     public void delete(K key) {
         if (cacheMap.get(key) == null) {
--- src/app_kvServer/persistence/LRUCache.java
+++ src/app_kvServer/persistence/LRUCache.java
@@ -5,11 +5,22 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.*;
 
+/**
+ * https://effective-java.com/2011/05/simple-lru-or-fifo-cache/
+ *
+ * @param <K> Type of the keys
+ * @param <V> Type of the values
+ */
+
 public class LRUCache<K,V> implements Cache<K,V>  {
     private static final Logger LOG = LogManager.getLogger(LRUCache.class);
 
     private final Map<K, V> cacheMap;
 
+    /**
+     * Default constructor
+     * @param cacheSize Number of elements to hold
+     */
     public LRUCache(int cacheSize) {
         this.cacheMap = new LinkedHashMap<>(cacheSize, 0.75f, true) {
             @Override
@@ -20,6 +31,11 @@ public class LRUCache<K,V> implements Cache<K,V>  {
         };
     }
 
+    /**
+     * {@inheritDoc}
+     * @param key
+     * @return key with its value
+     */
     @Override
     public V get(K key) {
         if (cacheMap.get(key) == null ) {
@@ -28,16 +44,30 @@ public class LRUCache<K,V> implements Cache<K,V>  {
         return cacheMap.get(key);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param key
+     * @param value
+     */
     @Override
     public  void put(K key, V value) {
         cacheMap.put(key, value);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param key
+     * @return boolean if the key is contained
+     */
     @Override
     public boolean contains(K key) {
         return cacheMap.containsKey(key);
     }
 
+    /**
+     * {@inheritDoc}
+     * @param key
+     */
     @Override
     public void delete(K key) {
         if (cacheMap.get(key) == null) {
