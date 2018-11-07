de36ac2faa244b5a589740ae67f4cda82909d7c6
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2018-11-07 23:17:45 +0100

    Setting up MS3 structure updates

---
+++ src/app_kvEcs/ECSClient.java
@@ -0,0 +1,16 @@
+package app_kvEcs;
+
+/**
+ * External Configuration Service (ECS)
+ */
+public class ECSClient {
+
+    /**
+     * Start the REPL.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Not implemented.");
+    }
+
+}
