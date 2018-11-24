899205fd825409e7212b613acdebecebaf17c56b
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2018-11-24 13:44:08 +0100

    Add performance measurement code

--- src/app_kvClient/performance/EnronIngester.java
+++ src/app_kvClient/performance/EnronIngester.java
@@ -1,4 +1,4 @@
-package testing.performance;
+package app_kvClient.performance;
 
 import client.KVStore;
 import client.exceptions.ClientException;
@@ -8,24 +8,42 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Performance testing utilities: Data ingestion for the Enron email data set.
+ * Arguments: seedNode:port data_root
+ */
 public class EnronIngester extends SimpleFileVisitor<Path> {
 
     private final Path root;
     private final KVStore client;
+    private final AtomicInteger fileCount;
+    private final AtomicLong dataSize;
 
     public EnronIngester(Path root, KVStore client) {
         this.root = root;
         this.client = client;
+        this.fileCount = new AtomicInteger(0);
+        this.dataSize = new AtomicLong(0);
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         Path relative = root.relativize(file);
-        String content = new String(Files.readAllBytes(file), StandardCharsets.UTF_8);
+        byte[] data = Files.readAllBytes(file);
+        fileCount.incrementAndGet();
+        dataSize.addAndGet(data.length);
+        String content = new String(data, StandardCharsets.UTF_8);
         try {
+            Instant start = Instant.now();
             KVMessage reply = client.put(relative.toString(), content);
-            System.out.println(relative + " " + reply.getStatus());
+            Instant end = Instant.now();
+            int latency = Duration.between(start, end).getNano();
+            System.out.println(latency);
         } catch (ClientException e) {
             e.printStackTrace();
         }
@@ -33,9 +51,10 @@ public class EnronIngester extends SimpleFileVisitor<Path> {
     }
 
     public static void main(String[] args) throws IOException {
-        KVStore client = new KVStore("localhost", 50000);
+        String[] parts = args[0].split(":");
+        KVStore client = new KVStore(parts[0], Integer.parseInt(parts[1]));
         client.connect();
-        Path root = Paths.get("/Volumes/Jeff External SSD/enron_maildir/");
+        Path root = Paths.get(args[1]);
         Files.walkFileTree(root, new EnronIngester(root, client));
         client.disconnect();
     }
